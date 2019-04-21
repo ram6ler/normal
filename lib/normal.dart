bbf720efaa3494e9f759ef647950ecdd1579dbcb
Richard Ambler <rambler@ibwya.net>
2019-04-21 12:27:13 +0800

    library documentation

--- lib/normal.dart
+++ lib/normal.dart
@@ -1,8 +1,10 @@
-/// Support for doing something awesome.
+/// Support for working with the normal probability distribution.
 ///
-/// More dartdocs go here.
+/// `normal` is a small and simple library that provides methods for
+/// Gaussian density, cumulative probability, quantiles and random
+/// number generation via an abstract class `Normal`, which contains
+/// static methods `pdf`, `cdf`, `quantile` (inverse cdf) and `generate`.
+///
 library normal;
 
 export 'src/normal_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
